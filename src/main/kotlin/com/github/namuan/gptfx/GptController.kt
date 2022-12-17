3f9233443d6fa29178a23553e855af8e3f9e0f23
namuan <575441+namuan@users.noreply.github.com>
2022-12-17 10:25:39 +0000

    feat: Update form to allow input and sending chat over to OpenAI

--- src/main/kotlin/com/github/namuan/gptfx/GptController.kt
+++ src/main/kotlin/com/github/namuan/gptfx/GptController.kt
@@ -1,26 +1,29 @@
 package com.github.namuan.gptfx
 
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.Button
 import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
 import java.io.File
 
 class GptController {
-    private val file: File = File(applicationDirectory, "notes.txt")
+    @FXML
+    private lateinit var btnSend: Button
 
     @FXML
-    private lateinit var notesTextArea: TextArea
+    private lateinit var txtPrompt: TextField
+
+    @FXML
+    private lateinit var txtChat: TextArea
+
+    private val file: File = File(applicationDirectory, "chat.txt")
 
     fun initialize() {
         file.parentFile.mkdirs()
-
-        if (file.exists()) {
-            notesTextArea.text = file.readText()
-            notesTextArea.positionCaret(notesTextArea.text.length)
-        }
     }
 
-    fun onKeyPressed() {
-        val notes = notesTextArea.text
-        file.writeText(notes)
+    fun onSendPrompt(actionEvent: ActionEvent) {
+
     }
 }
